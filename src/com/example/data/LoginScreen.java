3295fbd901ee6945128601096234f594642b6c24
James Foster <englandjf@yahoo.com>
2014-01-15 14:32:02 -0800

    Login function added

--- src/com/example/data/LoginScreen.java
+++ src/com/example/data/LoginScreen.java
@@ -1,8 +1,13 @@
 package com.example.data;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginScreen extends Activity {
 
@@ -10,6 +15,7 @@ public class LoginScreen extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login_screen);
+		loginUser();
 	}
 
 	@Override
@@ -17,6 +23,20 @@ public class LoginScreen extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login_screen, menu);
 		return true;
+	}
+	
+	private void loginUser(){
+		ParseUser.logInInBackground("test11", "test", new LogInCallback() {
+			public void done(ParseUser user, ParseException e) {
+				if(user != null){
+					Log.i("Test","Loggedin");
+				}
+				else{
+					Log.e("Error", "" + e.getMessage());
+				}
+				
+			}
+		});
 	}
 
 }
